c00dfdcac1953ce74e05cf2291d37928084e35ca
Thomas Deinhamer <thasmo@gmail.com>
2018-01-03 14:27:58 +0100

    Set leveldata-overrides correctly

--- configuration/up.sh
+++ configuration/up.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-LEVELDATA_CAVES="$(cat ${BASH_SOURCE%/*}/data/leveldataoverride.lua)" docker-compose up -d
+export LEVELDATA_OVERRIDES="$(cat ${BASH_SOURCE%/*}/data/leveldataoverride.lua)"
+docker-compose up -d
